b6bfb900a6d65cc63c8c8fb26dc009d173e40236
quinnyyy <quinnyyy@bu.edu>
2020-02-05 13:15:16 +0800

    Add packages

--- src/main/java/circus/animal/Bird.java
+++ src/main/java/circus/animal/Bird.java
@@ -1,5 +1,8 @@
-public class Bird extends Animal {
+package circus.animal;
+
+public abstract class Bird extends Animal {
     public void fly() {
         System.out.println("Whee ...");
     }
+
 }
